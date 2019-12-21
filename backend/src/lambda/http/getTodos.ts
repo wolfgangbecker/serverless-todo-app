c3e42b56d825387d01cf73588d956240038ccabd
Wolfgang Becker <wolfgang@cobalt.io>
2019-12-21 22:45:01 +0100

    Configure basic setup

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,4 +4,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } f
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  return Promise.resolve({
+    statusCode: 200,
+    body: "string"
+  })
 }
