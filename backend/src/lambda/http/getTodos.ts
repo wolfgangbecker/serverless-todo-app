e3f6ff1a2a557f553da5d5b05add67ec9d84560a
Wolfgang Becker <wolfgang@cobalt.io>
2019-12-22 00:50:16 +0100

    Implement getTodos endpoint

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,11 +1,39 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk';
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-  return Promise.resolve({
-    statusCode: 200,
-    body: "string"
-  })
+  const docClient = createDynamoDBClient();
+
+  try{
+    const result = await docClient.scan({
+      TableName: process.env.TODOS_TABLE
+    }).promise()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items)
+    }
+  } catch {
+    console.log("Error")
+
+    return {
+      statusCode: 404,
+      body: "string"
+    }
+  }
+}
+
+function createDynamoDBClient() {
+  if(process.env.IS_OFFLINE) {
+    console.log('Creating a local DynamoDB instance')
+
+    return new AWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+
+  return new AWS.DynamoDB.DocumentClient()
 }
