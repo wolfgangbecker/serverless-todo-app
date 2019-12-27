c15dcc49482c4876534e810ecccb4f45ca29517b
Wolfgang Becker <wolfgang@cobalt.io>
2019-12-27 10:05:50 -0600

    Simplify configuration

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = process.env.REACT_APP_API_ID
-const region = process.env.REACT_APP_REGION
-const stage = process.env.REACT_APP_STAGE
+const apiId = "82suylz0sj"
+const region = "eu-central-1"
+const stage = "dev"
 
-const domain = process.env.REACT_APP_DOMAIN
-const clientId = process.env.REACT_APP_CLIENT_ID
-const callbackUrl = process.env.REACT_APP_CALLBACK_URL
+const domain = "dev-9ncdqdat.eu.auth0.com"
+const clientId = "9349BqehLj3ZrZtISGqpchVGUJkC3BwE"
+const callbackUrl = "http://localhost:3000/callback"
 
 export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`
 
